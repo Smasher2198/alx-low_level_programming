1206060ea0bd2e44ae81b9b3b2bb0f3cf3d14c73
Smasher2198 <nickysmasher2198@gmail.com>
2023-06-15 19:08:35 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main . A  writing a program that print a put function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
