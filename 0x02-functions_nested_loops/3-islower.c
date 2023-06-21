196f2578df934c6853965f42a6b1bd97b780ba01
Smasher2198 <nickysmasher2198@gmail.com>
2023-06-21 15:32:18 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - displays 1 if the input is a, and 0 for others
+ * @c: The character in ASCII mode
+ *
+ * Return: 1 for lowercase characters, 0 for others
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	putchar('\n');
+}
