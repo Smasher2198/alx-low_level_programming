5cf8ffd13c0b4c59027ef66ea6007e9ff7f7e0d9
Smasher2198 <nickysmasher2198@gmail.com>
2023-06-21 07:28:12 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return 0(void);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
 }
-
-
--- 0x02-functions_nested_loops/putchar.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * main - A program that prints _putchar
- * Return: 1(success)
- */
-int _putchar(char c)
-{
-	return (write(l, 1, &c, 1);
-
