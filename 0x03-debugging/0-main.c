720ab48bf721472f79eabbedee61137a6e598ec0
Smasher2198 <nickysmasher2198@gmail.com>
2023-06-22 05:41:26 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Test for a positive and negative integer
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positve_or_negative(i);
+	return  (0);
+}
