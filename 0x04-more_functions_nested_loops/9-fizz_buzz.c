db132c4905a2f9aee4084da78593237a625eaef5
Smasher2198 <nickysmasher2198@gmail.com>
2023-06-23 19:40:40 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * print 1 to 100 in fizzbuzz rule
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 15 ==)
+			printf("fizzbuzz");
+		else if (i % 3 == 0)
+			printf(F"izz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i <100)
+			printf(" ");
+	}
+	printf('\n');
+	return (0);
+}
