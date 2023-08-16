9487ef48c9b216444a2f70c7fbaaa0d3570d2142
Smasher2198 <nickysmasher2198@gmail.com>
2023-08-15 17:30:14 -0700

    How many legs does a dog have if you call his tail a leg? Four. Saying that a tail is a leg doesn't make it a leg

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,66 +1,17 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include "dog.h"
 
 /**
- * struct dog_t - structure for a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * description: a structure representing a dogs attribute
- */
-typedef struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+* free_dog - frees memory allocated for a struct dog
+* @d: struct dog to free
+*/
 
-/**
- * free_dog - free memory allocated for a dog structure
- * @d: pointer to the dog structure
- *
- * description: the function takes a pointer to a dog structure
- * and frees the memory allocated for its name and owner fields,
- * as well as the memory for the dog structure itself
- */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
-	{
-		if (d->name != NULL)
-			free(d->name);
-		if (d->owner != NULL)
-			free(d->owner);
-		free(d);
-	}
-}
-int main(void)
+if (d)
 {
-	dog_t *my_dog = malloc(sizeof(dog_t));
-	if (my_dog == NULL)
-	{
-		perror("malloc failed");
-		return (1);
-	}
-	my_dog->name = malloc(sizeof(char) * 10);
-	if (my_dog->name == NULL)
-	{
-		perror("malloc failed");
-		free(my_dog);
-		return (1);
-	}
-	my_dog->owner = malloc(sizeof(char) * 10);
-	if (my_dog->owner == NULL)
-	{
-		perror("malloc failed");
-		free(my_dog->name);
-		free(my_dog);
-		return (1);
-	}
-	my_dog->age = 2.5;
-	my_dog->name = "Buddy";
-	my_dog->owner = "Alice";
-	free_dog(my_dog);
-	return (0);
+free(d->name);
+free(d->owner);
+free(d);
+}
 }
